e3428431a85ae645557ea5e5d2d6c4ff0476098f
froshroom <nicksoneclick@gmail.com>
2015-07-20 16:50:32 -0500

    True Initial Commit
    Converts code, framework with codemirror setup.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
+source 'https://rubygems.org'
+
 gem 'sinatra'
 gem 'slim'
-gem 'html2slim', '0.2.0'
-gem 'html2haml', '4.0.6'
+gem 'html2slim', '>=0.2.0'
+gem 'html2haml', '>=2.0.0'
+gem 'haml2slim', '>=0.4.7'
--- main.rb
+++ main.rb
@@ -3,6 +3,9 @@ require 'json'
 require 'slim'
 require 'html2slim'
 require 'html2haml'
+require 'haml2slim'
+require 'coderay'
+require 'pry'
 
 set :environment, :production
 set :port, '80'
@@ -10,20 +13,39 @@ set :static, true
 set :views, "views"
 
 get '/' do
-		conversions = ["erb", "haml", "html"]
+	conversions = ["haml->slim", "slim", "haml"]
     slim :index, :locals => {:conversions => conversions}
 end
 
-get '/convert.json' do
-		raw_text = params[:raw_text]
-		conversion_type = params[:convert_type]
-		
-		if conversion_method == 'erb'
-			converted_text = HTML2Slim.convert!(raw_text, conversion_type)
-		else 
-			converted_text = "something went wrong"
-		end
-		
-		content_type :json 
-		{:converted_txt => converted_txt}.to_json
+post '/convert.json' do
+	raw_text = params[:raw_text]
+	conversion_type = params[:convert_type]
+
+	converted_txt = convert(raw_text, conversion_type)
+
+	content_type :json 
+	{:converted_txt => converted_txt}.to_json
 end
+
+def convert(raw_text, conversion_type)
+	begin
+		case conversion_type
+		when "slim"
+			converted_txt = HTML2Slim.convert!(raw_text, conversion_type).to_s
+		when "haml->slim"
+			options = {:erb => true, :xhtml => false}
+			converted_txt = Html2haml::HTML.new(raw_text, options).render
+			converted_txt = Haml2Slim.convert!(converted_txt).to_s
+		when "haml"
+			options = {:erb => true, :xhtml => false}
+			converted_txt = Html2haml::HTML.new(raw_text, options).render
+		else 
+			converted_txt = "Converter not found!"
+		end
+	rescue Exception => e
+		return e.to_s
+	end	
+
+	return converted_txt
+end
+
